8298ba240ef988a1755cbc3edcb58ac6c89ae98d
Erika Vestin <erbj1201@student.miun.se>
2024-04-08 18:08:24 +0200

    lagt in bilder

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,12 +3,18 @@ import Navbar from "./MainNav";
 //Component
 const Header: React.FC = () => {
     return (
+      <div>
       <header className="container-fluid w-100 mx-auto border-bottom">
-        <a href="/" className="logo mx-auto">
-          Växa
-        </a>
+      <div>
         <Navbar />
+        </div>
+<nav className="navbar">
+  <a className="navbar-brand" href="/">
+    <img src="src\content\vaxa_thumbnail.png" height="30" alt="Växa Sverige" />
+  </a>
+</nav>
       </header>
+      </div>
     );
   };
   
