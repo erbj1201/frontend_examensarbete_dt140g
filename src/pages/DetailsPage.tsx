3c15d05d891e015fcd1d6338bcddfa5d3167b760
Sofia Dahlberg <sofiadahlberg@outlook.com>
2024-04-09 11:02:48 +0200

    getanimals klar

--- src/pages/DetailsPage.tsx
+++ src/pages/DetailsPage.tsx
@@ -1,19 +1,51 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-
+import React, { useState, useEffect } from "react";
 
 const DetailsPage: React.FC = () => {
+//Definera vilken typ av data som animals innehåller
+    interface Animal {
+        id: number;
+        animalId: string;
+        breed: string;
+        name: string;
+        imagepath: string;
+        herd_id: number;
+    }
+    const [animals, setAnimals] = useState<Animal[]>([]);
+
+    useEffect(() => {
+        const fetchAnimals = async () => {
+            try {
+                const response = await fetch("http://localhost:8000/api/animals")
+
+                if (response.ok) {
+                    const jsonData = await response.json();
+                    setAnimals(jsonData);
+                } else {
+                    throw new Error('Något gick fel');
+                }
+            } catch (error) {
+                console.error('Fel vid hämtning');
+            }
+        };
+        fetchAnimals();
+
+    }, []);
 
     return (
         <div>
             <Header />
             <main className="container mx-auto">
-        <h1>Detaljer</h1>
-
+                <h1>Detaljer</h1>
+                {animals && animals.map((animal) => (
+                    <div key={animal.id}>
+                        <p>{animal.animalId}</p><p>{animal.name}</p><p>{animal.breed}</p><p>{animal.herd_id}</p>
+                    </div>
+                ))}
             </main>
             <Footer />
         </div>
-    
-);
-};
+    );
+}
 export default DetailsPage;
