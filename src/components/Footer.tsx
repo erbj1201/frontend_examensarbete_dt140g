d8a3162efeba613604eb57ad9d75aeff12e74a70
Erika Vestin <erbj1201@student.miun.se>
2024-04-11 13:26:57 +0200

    footer ändras om inloggad/ej inloggad

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,15 @@
 //Footer.tsx
+
+import Cookies from "universal-cookie";
+
 //Component
-const Footer = () => {
+const Footer : React.FC = () => {
+  const cookies = new Cookies();
+  const token = cookies.get("token");
+
     return (
       <div>
+        {token ? (
         <footer className="container-fluid w-100 mx-auto border-top">
           <div className="d-flex justify-content-between ">
             <div className="mx-auto pt-5 ">
@@ -16,14 +23,19 @@ const Footer = () => {
             <div className="mx-auto pt-5">
           <p className="text-uppercase">Kontakta oss </p>
           <br />
-          <p className="text-left">Användarstöd <strong>010-471 09 07</strong> 
+          <p className="text-left p-3">Användarstöd <strong>010-471 09 07</strong> 
            <br />Kokontroll - Rättning <strong> 010 - 471 09 00</strong>
             <br />Kundsupport <strong> 010 - 471 06 60 </strong>
             </p>
           </div>
           </div>
-          <p className="text-center"> © Växa Sverige 2024 </p>
+          <p className="text-center mx-auto p-3"> © Växa Sverige 2024 </p>
         </footer>
+       ) : ( 
+          <footer className="container-fluid w-100 mx-auto border-top h-25">
+          <p className="text-center mx-auto p-3"> © Växa Sverige 2024 </p>
+        </footer>
+        )}
       </div>
     );
   };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,6 +21,7 @@ const Header: React.FC = () => {
 <SearchForm />
 <TokenCookie />
 <Logout />
+
       </header>
       </div>
     );
