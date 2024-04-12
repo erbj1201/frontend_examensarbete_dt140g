d3962f590b9f63f556c341dcb73aa6e89b64a73c
Erika Vestin <erbj1201@student.miun.se>
2024-04-12 11:34:02 +0200

    bytt namn på fetch-funktion

--- src/components/GetHerd.tsx
+++ src/components/GetHerd.tsx
@@ -1,14 +1,12 @@
 import React, { useEffect, useState } from "react";
 import Cookies from "universal-cookie";
-
 // Structure of herd
 interface Herd {
   id: number;
   herdId: string;
   address: string;
   userid: number;
-}    
-
+}
 // Structure of animal
 interface Animal {
   id: number;
@@ -17,7 +15,6 @@ interface Animal {
   name: string;
   herd_id: string;
 }
-
 // Create new instance of cookie
 const cookies = new Cookies();
 // Get token from cookies
@@ -30,59 +27,68 @@ const GetHerdComponent: React.FC = () => {
   const [error, setError] = useState<any>(undefined);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [selectedOption, setSelectedOption] = useState<string>("AllAnimals");
-  const [herds, setHerds] = useState<Herd[]>([]); 
-  const [animals, setAnimals] = useState<Animal[]>([]); 
+  const [herds, setHerds] = useState<Herd[]>([]);
+  const [animals, setAnimals] = useState<Animal[]>([]);
 
   // Fetch all herds and animals by user on component mount
   useEffect(() => {
-    fetchData();
+    fetchHerdsAnimals();
   }, []);
 
-    const fetchData = async () => {
-      try {
-        setIsLoading(true);
-        // Fetch all user herds
-        const herdsResponse = await fetch(`http://localhost:8000/api/herds/users/${userid}`, {
+  const fetchHerdsAnimals = async () => {
+    try {
+      setIsLoading(true);
+      // Fetch all user herds
+      const herdsResponse = await fetch(
+        `http://localhost:8000/api/herds/users/${userid}`,
+        {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`,
-            "Accept": "application/json",
-          }
-        });
-        const herdsData = await herdsResponse.json();
-        setHerds(herdsData);
+            Authorization: `Bearer ${token}`,
+            Accept: "application/json",
+          },
+        }
+      );
+      const herdsData = await herdsResponse.json();
+      setHerds(herdsData);
 
-        // Fetch all animals by user
-        const animalsResponse = await fetch(`http://localhost:8000/api/animals/users/${userid}`, {
+      // Fetch all animals by user
+      const animalsResponse = await fetch(
+        `http://localhost:8000/api/animals/users/${userid}`,
+        {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`,
-            "Accept": "application/json",
-          }
-        });
-        const animalsData = await animalsResponse.json();
-        setAnimals(animalsData);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
+            Authorization: `Bearer ${token}`,
+            Accept: "application/json",
+          },
+        }
+      );
+      const animalsData = await animalsResponse.json();
+      setAnimals(animalsData);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   // Fetch animals by selected herd
   const getAnimalsByHerd = async (id: string) => {
     try {
       setIsLoading(true);
-      const response = await fetch(`http://localhost:8000/api/animals/herds/${id}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${token}`,
-          "Accept": "application/json",
+      const response = await fetch(
+        `http://localhost:8000/api/animals/herds/${id}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+            Accept: "application/json",
+          },
         }
-      });
+      );
       const pickedAnimals = await response.json();
       setAnimals(pickedAnimals);
     } catch (error) {
@@ -92,14 +98,16 @@ const GetHerdComponent: React.FC = () => {
     }
   };
 
-  // Handle select change event
-  const handleSelectChange = async (event: React.ChangeEvent<HTMLSelectElement>) => {
-    //const selectedHerdId = event.target.value;
+  // Handle select change in select
+  const handleSelectChange = async (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
     const selectedOptionValue = event.target.value;
-    setSelectedOption(selectedOptionValue); // Uppdatera vald option
+    //update after choosen herd
+    setSelectedOption(selectedOptionValue);
     if (selectedOptionValue === "AllAnimals") {
       // If "AllAnimals" is selected, set animals to all animals
-     await fetchData();
+      await fetchHerdsAnimals();
     } else {
       // Fetch animals by selected herd
       getAnimalsByHerd(selectedOptionValue);
@@ -108,11 +116,18 @@ const GetHerdComponent: React.FC = () => {
 
   return (
     <div>
+      {" "}
+      {/*If there are more than zero herds*/}
       {!isLoading && !error && herds.length > 0 && (
         <div>
           <label htmlFor="herds">Välj en besättning:</label>
-          <br/>
-          <select id="herds" name="herds" onChange={handleSelectChange} value={selectedOption}>
+          <br />
+          <select
+            id="herds"
+            name="herds"
+            onChange={handleSelectChange}
+            value={selectedOption}
+          >
             <option value="AllAnimals">Visa alla djur</option>
             {herds.map((herd) => (
               <option key={herd.id} value={herd.id}>
@@ -134,9 +149,11 @@ const GetHerdComponent: React.FC = () => {
             </tr>
           </thead>
           <tbody>
+            {" "}
+            {/*Loop and write animals*/}
             {animals.map((animal) => (
               <tr key={animal.id}>
-                <td >{animal.id}</td>
+                <td>{animal.id}</td>
                 <td>{animal.animalId}</td>
                 <td>{animal.breed}</td>
                 <td>{animal.name}</td>
