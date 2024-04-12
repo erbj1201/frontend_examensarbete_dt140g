0a01bc615a22ab23b2cd6359531c05de4d5cc2f9
Erika Vestin <erbj1201@student.miun.se>
2024-04-12 11:30:17 +0200

    ok hämta anvs alla djur, alla anvs besättningar, filtrering efter besätt.

--- src/components/GetHerd.tsx
+++ src/components/GetHerd.tsx
@@ -1,32 +1,48 @@
 import React, { useEffect, useState } from "react";
 import Cookies from "universal-cookie";
-// const herdUrl = "; 
 
-
-
+// Structure of herd
 interface Herd {
   id: number;
   herdId: string;
   address: string;
-  user_id: number;
+  userid: number;
 }    
 
+// Structure of animal
+interface Animal {
+  id: number;
+  animalId: string;
+  breed: string;
+  name: string;
+  herd_id: string;
+}
+
+// Create new instance of cookie
+const cookies = new Cookies();
+// Get token from cookies
+const token = cookies.get("token");
+// Get userid from sessionstorage
+const userid = sessionStorage.getItem("userid");
+
 const GetHerdComponent: React.FC = () => {
+  // States
   const [error, setError] = useState<any>(undefined);
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [herds, setHerds] = useState<Herd[]>([]); // Här ändrades "herd" till "herds"
+  const [selectedOption, setSelectedOption] = useState<string>("AllAnimals");
+  const [herds, setHerds] = useState<Herd[]>([]); 
+  const [animals, setAnimals] = useState<Animal[]>([]); 
 
+  // Fetch all herds and animals by user on component mount
   useEffect(() => {
-    //Create new instance of cookie
-const cookies = new Cookies();
-//Get token from cookies
-const token = cookies.get("token");
-//Get userid from sessionstorage
-const userid = sessionStorage.getItem("userid");
-    const getHerds = async () => {
-      setIsLoading(true);
+    fetchData();
+  }, []);
+
+    const fetchData = async () => {
       try {
-        const response = await fetch(`http://localhost:8000/api/herds/users/${userid}`, {
+        setIsLoading(true);
+        // Fetch all user herds
+        const herdsResponse = await fetch(`http://localhost:8000/api/herds/users/${userid}`, {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
@@ -34,37 +50,104 @@ const userid = sessionStorage.getItem("userid");
             "Accept": "application/json",
           }
         });
-      
-        const pickedHerds = await response.json();
-        setHerds(pickedHerds);
-      
+        const herdsData = await herdsResponse.json();
+        setHerds(herdsData);
+
+        // Fetch all animals by user
+        const animalsResponse = await fetch(`http://localhost:8000/api/animals/users/${userid}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
+            "Accept": "application/json",
+          }
+        });
+        const animalsData = await animalsResponse.json();
+        setAnimals(animalsData);
       } catch (error) {
         setError(error);
       } finally {
         setIsLoading(false);
       }
     };
-    getHerds();
-  }, []);
+
+  // Fetch animals by selected herd
+  const getAnimalsByHerd = async (id: string) => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(`http://localhost:8000/api/animals/herds/${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`,
+          "Accept": "application/json",
+        }
+      });
+      const pickedAnimals = await response.json();
+      setAnimals(pickedAnimals);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  // Handle select change event
+  const handleSelectChange = async (event: React.ChangeEvent<HTMLSelectElement>) => {
+    //const selectedHerdId = event.target.value;
+    const selectedOptionValue = event.target.value;
+    setSelectedOption(selectedOptionValue); // Uppdatera vald option
+    if (selectedOptionValue === "AllAnimals") {
+      // If "AllAnimals" is selected, set animals to all animals
+     await fetchData();
+    } else {
+      // Fetch animals by selected herd
+      getAnimalsByHerd(selectedOptionValue);
+    }
+  };
 
   return (
     <div>
-      <h2>Besättningar</h2>
-      {!isLoading && !error && herds && herds.map((herd) => ( // Här ändrades "herd" till "herds"
-        <div key={herd.id} className="mx-auto text-center p-3">
-          <article className="mx-auto" key={herd.id}>
-            <h3>Besättningsid: {herd.herdId}</h3>
-            <p>
-              <strong>Id: {herd.id}</strong>
-              <br/>
-              <em>Adress: {herd.address}</em>
-            </p>
-            <p>
-              <strong>Användarid: {herd.user_id}</strong>
-            </p>
-          </article>
+      {!isLoading && !error && herds.length > 0 && (
+        <div>
+          <label htmlFor="herds">Välj en besättning:</label>
+          <br/>
+          <select id="herds" name="herds" onChange={handleSelectChange} value={selectedOption}>
+            <option value="AllAnimals">Visa alla djur</option>
+            {herds.map((herd) => (
+              <option key={herd.id} value={herd.id}>
+                Besättning: {herd.herdId}, {herd.address}
+              </option>
+            ))}
+          </select>
         </div>
-      ))}
+      )}
+      {animals.length > 0 ? (
+        <table className="table table-responsive table-hover">
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Djurid</th>
+              <th>Ras</th>
+              <th>Namn</th>
+              <th>Besättningsid</th>
+            </tr>
+          </thead>
+          <tbody>
+            {animals.map((animal) => (
+              <tr key={animal.id}>
+                <td >{animal.id}</td>
+                <td>{animal.animalId}</td>
+                <td>{animal.breed}</td>
+                <td>{animal.name}</td>
+                <td>{animal.herd_id}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>Inga djur i besättningen</p>
+      )}
     </div>
   );
 };
