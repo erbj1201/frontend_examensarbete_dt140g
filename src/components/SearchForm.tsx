381f07e1f9ae8d6f6cbf62ade53e7b45fd96bc5f
Erika Vestin <erbj1201@student.miun.se>
2024-05-03 13:51:06 +0200

    sök-knapp ok!

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -2,23 +2,23 @@
 const SearchForm = () => {
   return (
     <div className="mx-auto">
-      <form className="form-control form-control-sm mx-auto bglight border-0 w-50">
+      <form className="form-control form-control-sm align-items-center mx-auto bglight border-0 w-50">
         {/**search-input and button in header */}
         <div className="row mx-auto">
           <div className="col">
             <label>Sökord:</label>
-            <input type="text" className="form-control form-control-sm shadow-sm border-dark" />
+            <input type="text" className="form-control form-control-sm shadow border-dark" />
           </div>
           <div className="col">
             <label htmlFor="filter">Filtrera</label>
-            <select className="form-select form-select-sm shadow-sm border-dark">
+            <select className="form-select form-select-sm shadow border-dark">
               <option value={1}>1</option>
               <option value={2}>2</option>
               <option value={3}>3</option>
             </select>
             </div>
-            <div className="col d-flex align-items-center">
-            <button className="search-btn-btn d-flex align-items-center mx-auto p-2 shadow-sm">
+            <div className="col mt-3">
+            <button className="search-btn-btn mx-auto my-1 py-1 px-5 shadow border border-dark">
               <img
                 className="search-btn"
                 src="\src\content\search.png"
