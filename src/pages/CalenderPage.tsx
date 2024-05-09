2e81fcea2d13ff75fb4b272d42b84b35b7024306
Sofia Dahlberg <sofiadahlberg@outlook.com>
2024-05-09 11:27:31 +0200

    lag till kalender + nytt npmpaket

--- src/pages/CalenderPage.tsx
+++ src/pages/CalenderPage.tsx
@@ -2,11 +2,22 @@ import React, { useEffect} from 'react';
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import TokenCookie from "../components/TokenCookie";
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+
+import 'react-big-calendar/lib/css/react-big-calendar.css'
 
 
 const CalenderPage: React.FC = () => {
+  const localizer = momentLocalizer(moment)
+  //Start week with monday instead of sunday
 
-  
+
+  moment.locale('ko',{
+    week:{
+      dow : 1
+    }
+  });
   return (
     <div>
       {/*Including components*/}
@@ -14,6 +25,16 @@ const CalenderPage: React.FC = () => {
       <TokenCookie />
       <main className="container mx-auto">
         <h1>Kalender</h1>
+          <div>
+    <Calendar
+      localizer={localizer}
+      startAccessor="start"
+      endAccessor="end"
+     culture="sv-SE"
+      style={{ height: 500 }}
+    />
+  </div>
+
       </main>
       <Footer />
     </div>
