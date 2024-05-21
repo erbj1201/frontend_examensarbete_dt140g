13a1382b261151412cf6d614e3c50efc23fd89e2
Erika Vestin <erbj1201@student.miun.se>
2024-05-21 21:47:21 +0200

    footer längst ner

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,7 +15,7 @@ const Footer: React.FC = () => {
       {/*Check if there is token and show contactinfo and links to videos */}
       {token ? (
         <footer className="container-fluid w-100 mx-auto mt-5 shadow-sm border-top">
-          <div className="d-flex flex-column flex-sm-row justify-content-between  ">
+          <div className="d-flex flex-column flex-sm-row justify-content-between">
             <div className="mx-auto pt-5 ">
               <p className="text-uppercase">Länkar </p>
               <p className="text-left">
@@ -29,11 +29,8 @@ const Footer: React.FC = () => {
                 <a href="https://dreambroker.com/channel/npeiza94#/menu">
                   Instruktionsfilmer
                 </a>
-                
-               
               </p>
             </div>
-             
             <div className="mx-auto pt-5">
               <p className="text-uppercase">Kontakta oss </p>
               <br />
@@ -46,7 +43,7 @@ const Footer: React.FC = () => {
               </p>
             </div>
           </div>
-          <p className="vxa text-center mx-auto p-3"> © Växa Sverige 2024 </p>
+          <p className="vxa text-center mx-auto p-3 pb-5"> © Växa Sverige 2024 </p>
         </footer>
       ) : (
         //if no token, show just "växa"
