e53b1116a82cdd07ecc5dad853074129ae2fd7a9
Sofia Dahlberg <sofiadahlberg@outlook.com>
2024-05-27 19:57:42 +0200

    ändringar för user

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -9,10 +9,6 @@ interface User {
   id: string;
   imagepath: string;
 }
-//Structure of image
-interface Image {
-  imagepath: string;
-}
 
 export default function Account() {
   //States
@@ -34,10 +30,6 @@ export default function Account() {
     name: "",
     email: "",
     password: "",
-  });
-//State for edit image
-  const [image, setImage] = useState<Image>({
-    imagepath: "",
   });
 
   const [showMessage, setShowMessage] = useState<string | null>(null);
@@ -83,9 +75,8 @@ export default function Account() {
 
   const editImage = () => {
     setEditImageData(true);
-    setImage({
       imagepath: imageUrl || ""
-    });
+
   };
 
   // Function to clear update and delete messages after a specified time
